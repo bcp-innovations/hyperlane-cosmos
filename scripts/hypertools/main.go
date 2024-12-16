5c22b1a81f994f238dd6a3da6fc32f0c186d11be
mbreithecker <m@breithecker.de>
2024-12-16 14:35:40 +0100

    feat: add command for signing

--- scripts/hypertools/main.go
+++ scripts/hypertools/main.go
@@ -22,7 +22,7 @@ func main() {
 
 	rootCmd.AddCommand(&cobra.Command{
 		Use:   "warp-transfer [sender-contract] [recipient-contract] [recipient-user] [amount]",
-		Short: "Decodes a Hyperlane message into human readable format",
+		Short: "Creates a Warp message for sending tokens",
 		Args:  cobra.ExactArgs(4),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			amount, err := strconv.ParseUint(args[3], 10, 64)
@@ -30,6 +30,21 @@ func main() {
 				return err
 			}
 			return GenerateWarpTransfer(args[0], args[1], args[2], amount)
+		}})
+
+	rootCmd.PersistentFlags().StringArray("private-keys", PRIVATE_KEYS, "Enter custom private keys")
+
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "sign [message]",
+		Short: "Signs a message with the given private keys",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			signature, err := signMessage(args[0])
+			if err != nil {
+				return err
+			}
+			fmt.Println(signature)
+			return nil
 		}})
 
 	if err := rootCmd.Execute(); err != nil {
