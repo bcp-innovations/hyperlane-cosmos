97881487826fac62eb4eaeefe7c958506ded3ad6
christopherbrumm <christopherbrumm2000@gmail.com>
2025-01-28 16:12:17 +0100

    refactor: params endpoint

--- x/ism/types/query.pb.gw.go
+++ x/ism/types/query.pb.gw.go
@@ -448,7 +448,7 @@ var (
 
 	pattern_Query_Ism_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"hyperlane", "v1", "isms", "id"}, "", runtime.AssumeColonVerbOpt(false)))
 
-	pattern_Query_Params_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"hyperlane", "v1", "params"}, "", runtime.AssumeColonVerbOpt(false)))
+	pattern_Query_Params_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"hyperlane", "ism", "v1", "params"}, "", runtime.AssumeColonVerbOpt(false)))
 
 	pattern_Query_VerifyDryRun_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 1, 0, 4, 1, 5, 4, 1, 0, 4, 1, 5, 5}, []string{"hyperlane", "v1", "verify_dry_run", "ism_id", "message", "metadata"}, "", runtime.AssumeColonVerbOpt(false)))
 )
