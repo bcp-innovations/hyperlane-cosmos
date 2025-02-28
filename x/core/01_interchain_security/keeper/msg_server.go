4ec96bedaf662e49bcc3293765da42fc47547aa8
mbreithecker <m@breithecker.de>
2025-02-28 10:46:56 +0100

    chore: WIP

--- x/core/01_interchain_security/keeper/msg_server.go
+++ x/core/01_interchain_security/keeper/msg_server.go
@@ -24,6 +24,9 @@ func NewMsgServerImpl(keeper *Keeper) types.MsgServer {
 	return &msgServer{k: keeper}
 }
 
+// AnnounceValidator lets a validator store a string in the state, which is queryable.
+// The string should contain the storage location for the proofs (e.g. an S3 bucket)
+// The Relayer uses this information to fetch the signatures for messages.
 func (m msgServer) AnnounceValidator(ctx context.Context, req *types.MsgAnnounceValidator) (*types.MsgAnnounceValidatorResponse, error) {
 	if req.Validator == "" {
 		return nil, fmt.Errorf("validator cannot be empty")
@@ -166,14 +169,13 @@ func (m msgServer) CreateNoopIsm(ctx context.Context, ism *types.MsgCreateNoopIs
 	if err != nil {
 		return nil, err
 	}
-	if err != nil {
-		return nil, err
-	}
 
 	newIsm := types.NoopISM{
 		Id:    ismId.String(),
 		Owner: ism.Creator,
 	}
+
+	// no validation needed, as there are no params to this ism
 
 	if err = m.k.isms.Set(ctx, ismId.Bytes(), &newIsm); err != nil {
 		return nil, err
