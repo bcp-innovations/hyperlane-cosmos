bcfb19094414cdd3083d2a14d9255a10fc1f4f9e
yjamin <kaminund@gmail.com>
2025-03-03 14:42:41 +0100

    fix: start proto at 0

--- x/warp/types/errors.go
+++ x/warp/types/errors.go
@@ -3,6 +3,6 @@ package types
 import "cosmossdk.io/errors"
 
 var (
-	ErrNotEnoughCollateral = errors.Register(ModuleName, 2, "not enough collateral")
-	ErrTokenNotFound       = errors.Register(ModuleName, 3, "token not found")
+	ErrNotEnoughCollateral = errors.Register(ModuleName, 1, "not enough collateral")
+	ErrTokenNotFound       = errors.Register(ModuleName, 2, "token not found")
 )
