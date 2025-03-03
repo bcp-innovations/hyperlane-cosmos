2c20de7ffe6875ef18167251778c0f1f00dbdc55
yjamin <kaminund@gmail.com>
2025-03-03 13:57:55 +0100

    chore: remove dispatch tx

--- x/warp/types/errors.go
+++ x/warp/types/errors.go
@@ -2,4 +2,7 @@ package types
 
 import "cosmossdk.io/errors"
 
-var ErrNotEnoughCollateral = errors.Register(ModuleName, 2, "not enough collateral")
+var (
+	ErrNotEnoughCollateral = errors.Register(ModuleName, 2, "not enough collateral")
+	ErrTokenNotFound       = errors.Register(ModuleName, 3, "token not found")
+)
