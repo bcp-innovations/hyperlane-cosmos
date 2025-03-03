a358cb21f8643acae41e2e766dc93500e8bb2139
christopherbrumm <christopherbrumm2000@gmail.com>
2025-03-03 09:04:33 +0100

    chore: add comments

--- util/post_dispatch_hook.go
+++ util/post_dispatch_hook.go
@@ -6,11 +6,16 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// StandardHookMetadata is application specific, thereby different of the EVM implementation.
+// StandardHookMetadata is application specific, thereby different to the EVM implementation.
 type StandardHookMetadata struct {
+	// Address is used to determine the message sender.
+	// E.g., IGP uses the address to determine the fee payer.
 	Address sdk.AccAddress
 
+	// GasLimit of the destination transaction.
 	GasLimit math.Int
 
+	// CustomHookMetadata can be used to pass custom data to a PostDispatch hook.
+	// The hook is responsible for validating and decoding the custom metadata.
 	CustomHookMetadata []byte
 }
