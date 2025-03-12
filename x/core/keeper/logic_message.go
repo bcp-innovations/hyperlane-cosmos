84cd797948a14e29c027843058b972e9d593c3c4
christopherbrumm <christopherbrumm2000@gmail.com>
2025-03-12 22:57:48 +0100

    fix: validate `message.Destination` matches `mailbox.LocalDomain`

--- x/core/keeper/logic_message.go
+++ x/core/keeper/logic_message.go
@@ -24,6 +24,10 @@ func (k Keeper) ProcessMessage(ctx sdk.Context, mailboxId util.HexAddress, rawMe
 	}
 	mailbox.MessageReceived++
 
+	if message.Destination != mailbox.LocalDomain {
+		return fmt.Errorf("message destination %v does not match local domain %v", message.Destination, mailbox.LocalDomain)
+	}
+
 	err = k.Mailboxes.Set(ctx, mailboxId.GetInternalId(), mailbox)
 	if err != nil {
 		return err
